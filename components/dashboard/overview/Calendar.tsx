b93f96b877f22d4a949505671e3cf8086febc797
Abdulgafar Temitope Tajudeen <attajudeen@myseneca.ca>
2025-03-31 20:25:24 -0400

    Add supabase keys

--- components/dashboard/overview/Calendar.tsx
+++ components/dashboard/overview/Calendar.tsx
@@ -4,28 +4,24 @@ import * as React from "react";
 import { Calendar } from "@/components/ui/calendar";
 
 interface CalendarComponentProps {
-  onSelectDate: (date: string) => void; // callback to handle the selected date
+  onSelectDate: (date: string) => void;
 }
 
 export function CalendarComponent({ onSelectDate }: CalendarComponentProps) {
   const [date, setDate] = React.useState<Date | undefined>(new Date());
 
   const handleDateSelect = (selectedDate: Date) => {
-    setDate(selectedDate); // Update local state with the selected date
-    onSelectDate(selectedDate.toISOString().split('T')[0]); // Send the selected date in 'YYYY-MM-DD' format
+    setDate(selectedDate); 
+    onSelectDate(selectedDate.toISOString().split('T')[0]); 
   };
 
   return (
     <div>
-      <div className="flex items-center justify-between mb-4">
-        <h3 className="font-medium">Select a Date</h3>
-      </div>
-
       <Calendar
-        mode="single" // Single date selection mode
-        selected={date} // Bind the selected date to the state
-        onSelect={handleDateSelect} // Handler to manage date selection
-        className="rounded-md border shadow" // Tailwind classes for styling
+        mode="single"
+        selected={date} 
+        onSelect={handleDateSelect} 
+        className="rounded-md border shadow" 
       />
     </div>
   );
--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const SUPABASE_URL="https://mvqgdppbfikhcbwgkskx.supabase.co"
+const SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12cWdkcHBiZmlraGNid2drc2t4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI2ODAwMTEsImV4cCI6MjA1ODI1NjAxMX0.KABeGhRwaWsqoQbymXwy_wONZnOa2P_BTcNMeP_fPiU"
+
+const supabaseUrl = SUPABASE_URL!;
+const supabaseAnonKey = SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
