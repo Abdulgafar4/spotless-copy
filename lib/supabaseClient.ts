4d1f1d5eb369b23e57b2d594bd4d7c14579846b6
Abdulgafar Temitope Tajudeen <attajudeen@myseneca.ca>
2025-04-02 11:23:27 -0400

    change supabase key

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL="https://mvqgdppbfikhcbwgkskx.supabase.co"
-const SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12cWdkcHBiZmlraGNid2drc2t4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI2ODAwMTEsImV4cCI6MjA1ODI1NjAxMX0.KABeGhRwaWsqoQbymXwy_wONZnOa2P_BTcNMeP_fPiU"
+const SUPABASE_URL="https://xehwtihslletcqcxaust.supabase.co"
+const SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhlaHd0aWhzbGxldGNxY3hhdXN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMxODU2MTYsImV4cCI6MjA1ODc2MTYxNn0.MKjFvmI2Bx4naFbxMPoDyR0UL6e2lIm6jG92Y9o_Wts"
 
 const supabaseUrl = SUPABASE_URL!;
 const supabaseAnonKey = SUPABASE_ANON_KEY!;
